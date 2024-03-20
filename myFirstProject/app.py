bd11cc32b7124c8bb923ca6a33fe5e228a48f463
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-20 23:31:36 +0330

    Update app.py

--- myFirstProject/app.py
+++ myFirstProject/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def  home():
-    return 'hello world!'
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
