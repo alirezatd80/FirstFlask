33f815efb7870543d49daaef690ec7b1ed178d05
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-20 23:25:49 +0330

    nothings

--- myFirstProject/app.py
+++ myFirstProject/app.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def  home():
     return 'hello world!'
+
 if __name__ == "__main__":
     app.run(debug=True)
