bfe71bb36b90d2315487693e14520e0d8c40f207
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-20 23:28:38 +0330

    Revert "Update app.py"
    This reverts commit 61ed81e31a4ddfa2f88bb56f730ca3e8d1de3b81.

--- myFirstProject/app.py
+++ myFirstProject/app.py
@@ -1,11 +1,11 @@
-from flask import Flask ,render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def  home():
-    return render_template('home.html')
+    return 'hello world!'
 
 if __name__ == "__main__":
     app.run(debug=True)
